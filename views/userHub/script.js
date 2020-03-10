de1fb108347a255fc26faef795899abce6ff0afa
ebenFl <ebfl1466@colorado.edu>
2020-03-10 17:35:01 -0500

    added in userHub, loginPage, all with appropriate links

---
+++ views/userHub/script.js
@@ -0,0 +1,10 @@
+function validate(){
+  console.log("Called validate")
+  var email = document.getElementById("inputEmail");
+  var password = document.getElementById("inputPassword");
+  console.log(inputEmail.value);
+  console.log(password.value);
+  if(email.value && password.value){
+    window.location.href = "userHub.html";
+  }
+}
